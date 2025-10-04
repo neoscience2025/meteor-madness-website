15d7b3969fb5c174067f1e2d152378605ec2e53a
Oscar Chiriboga <ochiribogaz@alumni.usfq.edu.ec>
2025-10-04 11:05:55 -0500

    Add i18n localization support

---
+++ i18nConfig.ts
@@ -0,0 +1,10 @@
+const defaultLocale = "es"
+const locales = [defaultLocale, "en", "qu"];
+
+const i18nConfig = {
+    locales,
+    defaultLocale,
+    prefixDefault: true,
+}
+
+export default i18nConfig;
