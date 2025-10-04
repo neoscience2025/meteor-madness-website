6f4baf1737b0dc2d42f406c32d7f06a58c50309f
Oscar Chiriboga <ochiribogaz@alumni.usfq.edu.ec>
2025-10-04 11:19:07 -0500

    ESLint and Next config fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://flagcdn.com/w20/**')],
+  },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
