88010924ab9106a4879e88452987b537255a408a
Oscar Chiriboga <ochiribogaz@alumni.usfq.edu.ec>
2025-10-04 15:28:43 -0500

    Add leaflet css

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../globals.css";
+import "leaflet/dist/leaflet.css";
 import i18nConfig from "../../../i18nConfig";
 import initTranslations from "../i18n";
 import { NeoscienceParams } from "@/interfaces/common";
