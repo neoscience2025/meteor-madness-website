12007d0f51b405ca3bcf35a7e53a07ab16a937e7
Oscar Chiriboga <ochiribogaz@alumni.usfq.edu.ec>
2025-10-04 13:45:12 -0500

    Add story-telling page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,7 +7,7 @@ import PreviewSection from "@/components/PreviewSection";
 
 
 const NAMESPACES_REQUIRED = [
-  "team"
+  "team",
 ];
 
 export default async function Home({ params }) {
@@ -19,12 +19,9 @@ export default async function Home({ params }) {
     locale={locale}
     resources={resources}
   >
-    <main>
-      <Banner />
-      <MottosSection />
-      <OurTeam />
-      <PreviewSection />
-    </main>
-
+    <Banner />
+    <MottosSection />
+    <OurTeam />
+    <PreviewSection />
   </TranslationsProvider>
 }
---
+++ src/app/[locale]/story-telling/page.tsx
@@ -0,0 +1,23 @@
+import TranslationsProvider from "@/components/translation-provider";
+import initTranslations from "@/app/i18n";
+import Banner from "@/components/Banner";
+import MottosSection from "@/components/MottosSection";
+import OurTeam from "@/components/OurTeam";
+import PreviewSection from "@/components/PreviewSection";
+
+const NAMESPACES_REQUIRED = [
+    "team"
+];
+
+export default async function Home({ params }) {
+    const { locale } = await params;
+    const { t, resources, } = await initTranslations(locale, NAMESPACES_REQUIRED);
+
+    return <TranslationsProvider
+        namespaces={NAMESPACES_REQUIRED}
+        locale={locale}
+        resources={resources}
+    >
+        aqui va el libro
+    </TranslationsProvider>
+}
