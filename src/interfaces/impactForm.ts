a9bef9543b93a339dd1ba3a4b0553c81db076377
Oscar Chiriboga <ochiribogaz@alumni.usfq.edu.ec>
2025-10-04 19:49:51 -0500

    Add MeteoriteSelector

--- src/interfaces/impactForm.ts
+++ src/interfaces/impactForm.ts
@@ -1,9 +1,12 @@
+import { MeteoriteName } from './meteorites';
+
 export interface ImpactFormData {
     latitude: number;
     longitude: number;
     diameter: number;
     speed: number;
     impactAngle: number;
+    meteoriteType: MeteoriteName;
 }
 
 export type CalculationData = Omit<ImpactFormData, "latitude" | "longitude">;
