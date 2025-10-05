6295085a41e244fab723b28f01f958c679458d47
Oscar Chiriboga <ochiribogaz@alumni.usfq.edu.ec>
2025-10-04 19:13:00 -0500

    Add meteorites assets

---
+++ src/lib/map/meteorites.ts
@@ -0,0 +1,22 @@
+const METEORITE_OPTIONS = {
+    gold: {
+        name: "gold",
+        density: 19320,
+    },
+    iron: {
+        name: "iron",
+        density: 7860,
+    },
+    carbon: {
+        name: "carbon",
+        density: 2000,
+    },
+    comet: {
+        name: "comet",
+        density: 750,
+    },
+    stone: {
+        name: "stone",
+        density: 3000,
+    },
+};
